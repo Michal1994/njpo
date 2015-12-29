b67435389f4a1b880ffaed1c8e6cad3e34ab0fe0
Michal1994 <pielkus@o2.pl>
2015-12-29 17:13:30 +0100

    Tests update

--- src/tests/RandomArrayGeneratorTest.java
+++ src/tests/RandomArrayGeneratorTest.java
@@ -4,10 +4,19 @@ import static org.junit.Assert.*;
 
 import java.util.Arrays;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class RandomArrayGeneratorTest {
-
+	
+	static long start;
+	
+	@Before
+	public void start(){
+		start = System.nanoTime();
+	}
+	
 	@Test
 	public void testGenerate() {
 		int[] a = RandomArrayGenerator.generate();
@@ -21,6 +30,11 @@ public class RandomArrayGeneratorTest {
 		Arrays.sort(b);
 		RandomArrayGenerator.sort(a, 0, a.length-1);
 		assertArrayEquals(b, a);
+	}
+	
+	@After
+	public void end(){
+		System.out.println( (System.nanoTime() - start)/1000000 + "ms" );
 	}
 
 }
